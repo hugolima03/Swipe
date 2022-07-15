2a34396b889856bedf2be29230918dd67f39cea6
Hugo Lima Romão <hugo8romao@gmail.com>
2022-07-14 22:26:42 -0400

    FIX: Card type

--- src/components/Card/stories.tsx
+++ src/components/Card/stories.tsx
@@ -1,13 +1,19 @@
 import { Story, Meta } from '@storybook/react'
-import Card from '.'
+import Card, { CardProps } from '.'
 
 export default {
   title: 'Card',
   component: Card,
   parameters: {
     layout: 'fullscreen',
-    viewport: 'defaultViewport'
+    viewport: 'defaultViewport',
+    args: {
+      id: '1',
+      answer: { html: 'answer' },
+      onSwipe: () => ({}),
+      question: 'questiong'
+    } as CardProps
   }
 } as Meta
 
-export const Default: Story = (args) => <Card {...args} />
+export const Default: Story<CardProps> = (args) => <Card {...args} />
